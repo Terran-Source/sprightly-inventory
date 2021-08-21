ad8cba7520aacb52538cce07ae9e06047fb1a961
Tamal Patra <patra.tamal@gmail.com>
2021-08-21 16:49:33 +0530

    cleanup

--- src/sprightly_inventory/lib/extensions/extensions.dart
+++
@@ -1,8 +0,0 @@
-library marganam.extensions;
-
-export 'enum_extensions.dart';
-export 'file_system_entity_extensions.dart';
-export 'http_response_extensions.dart';
-export 'json_extensions.dart';
-export 'moor/enum_type_converter.dart';
-export 'string_extensions.dart';
--- src/sprightly_inventory/lib/extensions/file_system_entity_extensions.dart
+++
@@ -1,9 +0,0 @@
-library marganam.extensions;
-
-import 'dart:io';
-import 'package:path/path.dart' as p;
-
-extension FileSystemExtension on FileSystemEntity {
-  String get name => p.basename(path);
-  String get basePath => p.dirname(path);
-}
