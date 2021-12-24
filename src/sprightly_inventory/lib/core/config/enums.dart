b8cdd5c333f3ee046dcc37eb76479b2970a8c460
Tamal Patra <patra.tamal@gmail.com>
2021-12-25 02:28:34 +0530

    enumTypes is now part of `package:dart_marganam/db.dart`

--- src/sprightly_inventory/lib/core/config/enums.dart
+++ src/sprightly_inventory/lib/core/config/enums.dart
@@ -1,17 +1,19 @@
-const Map<Type, List> enumTypes = {
-  MemberIdType: MemberIdType.values,
-  GroupType: GroupType.values,
-  AccountType: AccountType.values,
-  CategoryType: CategoryType.values,
-  TransactionTag: TransactionTag.values,
-  GroupActivityType: GroupActivityType.values,
-  FontType: FontType.values,
-  FontStyle: FontStyle.values,
-  ThemeMode: ThemeMode.values,
-  PropertyType: PropertyType.values,
-  ResourceFrom: ResourceFrom.values,
-  Environment: Environment.values,
-};
+import 'package:dart_marganam/db.dart';
+
+void updateDbEnumTypes() {
+  enumTypes[MemberIdType] = MemberIdType.values;
+  enumTypes[GroupType] = GroupType.values;
+  enumTypes[AccountType] = AccountType.values;
+  enumTypes[CategoryType] = CategoryType.values;
+  enumTypes[TransactionTag] = TransactionTag.values;
+  enumTypes[GroupActivityType] = GroupActivityType.values;
+  enumTypes[FontType] = FontType.values;
+  enumTypes[FontStyle] = FontStyle.values;
+  enumTypes[ThemeMode] = ThemeMode.values;
+  enumTypes[PropertyType] = PropertyType.values;
+  enumTypes[ResourceFrom] = ResourceFrom.values;
+  enumTypes[Environment] = Environment.values;
+}
 
 /// Database enums
 ///
