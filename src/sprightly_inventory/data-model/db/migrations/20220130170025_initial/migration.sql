ad01597ea82b519ae288dafa8e71b1727b624d54
Tamal Patra <patra.tamal@gmail.com>
2022-01-30 22:31:29 +0530

    initial migration

---
+++ src/sprightly_inventory/data-model/db/migrations/20220130170025_initial/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" BIGINT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "authorId" BIGINT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" BIGINT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "role" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
